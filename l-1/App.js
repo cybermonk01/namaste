a778730687543424198bcd571466f1c8daf07ee1
Ashutosh Singh <singh.saahab08@gmail.com>
2023-07-18 11:55:37 +0530

    react elements

--- l-1/App.js
+++ l-1/App.js
@@ -4,5 +4,30 @@ const heading = React.createElement(
   "Hello world react cdn"
 );
 
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+
+  [
+    React.createElement(
+      "div",
+      { id: "child" },
+      [
+        React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h2", {}, "I am an h2 tag"),
+      ] //if you want multiple child pass into array
+    ),
+    React.createElement(
+      "div",
+      { id: "child2" },
+      [
+        React.createElement("h1", {}, "I am an h1 tag"),
+        React.createElement("h2", {}, "I am an h2 tag"),
+      ] //if you want multiple child pass into array
+    ),
+  ]
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
+root.render(parent);
